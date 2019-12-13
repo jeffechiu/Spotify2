b0bec4910c03bb654a81d36378113f307cb40735
Jeffrey Chiu <jchiu2@andrew.cmu.edu>
2019-12-13 13:24:53 -0800

    added a to do file

--- playlists.js
+++ playlists.js
@@ -1,5 +1,7 @@
 var request = require("request");
+//Jeffrey's Spotify User ID
 var user_id = "1215747131";
+//Access token to be changed every time
 var token = "Bearer BQBfQK0r3q4WfE5zBFwrOE-LS2DhqFEEKs_yYk5fZHk-QetjpWQgR8dFuZWwYWOdBWroIfhy4rQ66jYqt448K7Kv6dW-hm0SUmsxEup-j5_smh2cP4gmWRE0jPYUEwpHJxx69AGuj_tOiIhFNCBdNBB92qDYWNc";
 var playlists_url = "https://api.spotify.com/v1/users/"+user_id+"/playlists";
 
