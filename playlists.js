374afa55714432cc565064eadf57cbfb8bf3a64a
Jeffrey Chiu <jchiu2@andrew.cmu.edu>
2019-12-14 00:43:22 -0800

    moved everything to python/spotipy, enabled track analysis

--- playlists.js
+++ playlists.js
@@ -2,24 +2,33 @@ var request = require("request");
 //Jeffrey's Spotify User ID
 var user_id = "1215747131";
 //Access token to be changed every time
-var token = "Bearer BQBfQK0r3q4WfE5zBFwrOE-LS2DhqFEEKs_yYk5fZHk-QetjpWQgR8dFuZWwYWOdBWroIfhy4rQ66jYqt448K7Kv6dW-hm0SUmsxEup-j5_smh2cP4gmWRE0jPYUEwpHJxx69AGuj_tOiIhFNCBdNBB92qDYWNc";
+var token = "Bearer BQCH68Lo48SQAJjRXsP4FdgT7VBPGyJP7yz8qzHaMA1rRi3Jfr1HYiOy3VJDbjeP-VgYWXGC1-p6jmEHlwGCB3yM2mrP0wqqql2rrhaDODimLQmaGaPGsiAmorl9Buz22uy-Pg8509pVQ05vPw";
 var playlists_url = "https://api.spotify.com/v1/users/"+user_id+"/playlists";
+var tracks = [];
 
 request({url: playlists_url, headers:{"Authorization":token}}, function(err, res){
 	if (res){
 		var playlists = JSON.parse(res.body);
 		//console.log(JSON.stringify(playlists.items, null, " "));
+		//get first playlist in list of playlists
 		var playlist_url = playlists.items[0].href;
 		request({url: playlist_url, headers:{"Authorization":token}}, function(err, res){
 			if (res){
+				//get all tracks in a playlist and add them to an array variable
 				var playlist = JSON.parse(res.body);
+				//var trks=[];
 				console.log("playlist: "+playlist.name);
 				//console.log(playlist.tracks)
 				playlist.tracks.items.forEach(function(track){
-					console.log(track.track.name);
+					tracks.push(track.track);
 				});
 				//playlist.tracks.forEach(track => console.log(track.track.name));
+				console.log(tracks.length);
+				//tracks = trks;
+				//postman.setGlobalVariable("trks", trks)
 			}
 		})
+		//console.log(trks);
 	}
-})
+});
+console.log(tracks.length);
---
+++ playlists.py
@@ -0,0 +1,39 @@
+import sys
+import spotipy
+import spotipy.util as util
+
+username = "1215747131"
+client_id = "d3a608860b4f4442ad2b0aebba6afeaa"
+client_secret = "2daa692e01234f9ca180f4dccef200f6"
+redirect_uri = "https://www.spotify.com/us/"
+scope = "playlist-read-private"
+
+token = util.prompt_for_user_token(username, scope = scope, client_id = client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+
+def get_playlist(sp):
+	playlists = sp.user_playlists(username)
+	for playlist in playlists['items']:
+		if playlist['name'] == '318 Playlist':
+			p = playlist
+	return p
+
+def get_tracks(sp, playlist):
+	tracks = []
+	trks = sp.user_playlist_tracks(username, playlist['id'])
+	#print(type(trks))
+	for song in trks['items']:
+		tracks += [song['track']]
+		#print(song['track']['name'])
+	return tracks
+
+
+if token:
+	sp=spotipy.Spotify(auth=token)
+	p = get_playlist(sp)
+	#print(p)
+	#print(type(p))
+	trks = get_tracks(sp, p)
+	#print(trks)
+else:
+	print("can't get token for", username)
+
