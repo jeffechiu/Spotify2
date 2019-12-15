92d1c540d9854c8c70ec91c4bcab0b3ad609a231
Jeffrey Chiu <jchiu2@andrew.cmu.edu>
2019-12-14 16:48:17 -0800

    recommendation system works, need to find way to display

--- playlists.py
+++ playlists.py
@@ -1,6 +1,7 @@
 import sys
 import spotipy
 import spotipy.util as util
+import operator
 
 username = "1215747131"
 client_id = "d3a608860b4f4442ad2b0aebba6afeaa"
@@ -30,7 +31,6 @@ def get_sets_of_5(sp, tracks):
 	ans = []
 	loops = len(tracks)-4
 	trlen = len(tracks)
-	print(trlen)
 	for i1 in range(0, loops):
 		for i2 in range(i1+1, min(i1+1+loops, trlen)):
 			for i3 in range(i2+1, min(i2+1+loops, trlen)):
@@ -88,6 +88,35 @@ def get_popularity(sp, tracks):
 def get_recs(sp, ids, recats):
 	return sp.recommendations(seed_tracks=ids, country="US", limit=10, min_acousticness=recats[0][0], max_acousticness=recats[0][1], target_acousticness=recats[0][2], min_danceability=recats[1][0], max_danceability=recats[1][1], target_danceability=recats[1][2], min_duration_ms=recats[2][0], max_duration_ms=recats[2][1], target_duration_ms=round(recats[2][2]), min_energy=recats[3][0], max_energy=recats[3][1], target_energy=recats[3][2], min_instrumentalness=recats[4][0], max_instrumentalness=recats[4][1], target_instrumentalness=recats[4][2], min_key=recats[5][0], max_key=recats[5][1], target_key=round(recats[5][2]), min_liveness=recats[6][0], max_liveness=recats[6][1], target_liveness=recats[6][2], min_loudness=recats[7][0], max_loudness=recats[7][1], target_loudness=recats[7][2], min_mode=recats[8][0], max_mode=recats[8][1], target_mode=round(recats[8][2]), min_speechiness=recats[9][0], max_speechiness=recats[9][1], target_speechiness=recats[9][2], min_tempo=recats[10][0], max_tempo=recats[10][1], target_tempo=recats[10][2], min_time_signature=recats[11][0], max_time_signature=recats[11][1], target_time_signature=round(recats[11][2]), min_valence=recats[12][0], max_valence=recats[12][1], target_valence=recats[12][2], min_popularity=recats[13][0], max_popularity=recats[13][1], target_popularity=round(recats[13][2]))
 
+def get_recs_all(sp, allPoss):
+	ans = dict()
+	attributes = ["acousticness", "danceability", "duration_ms", "energy", "instrumentalness", "key", "liveness", "loudness", "mode", "speechiness", "tempo", "time_signature", "valence"]
+	for poss in allPoss:
+		ids = audio_id(sp, poss)
+		features = audio_analysis(sp, ids)
+		lenat = len(attributes)
+		recats = []
+		for i in range(lenat):
+			recats += [get_attributes(sp, features, attributes[i])]
+		recats += [get_popularity(sp, trks)] 
+		recs = get_recs(sp, ids, recats)
+		for track in recs['tracks']:
+			if track not in ans:
+				ans[track] = 0
+			ans[track] += 1
+		print(ans)
+	return ans
+
+def display(recs):
+	l = sorted(recs.items(), key=operator.itemgetter(1), reverse=True)
+	ans = '''
+	Top 10 Recommended Songs
+
+	'''
+	return ans
+
+
+
 if token:
 	sp=spotipy.Spotify(auth=token)
 	p = get_playlist(sp)
@@ -97,19 +126,13 @@ if token:
 	#print(trks)
 	#print(len(trks))
 	#print(type(trks))
-	ans = get_sets_of_5(sp, trks)
+	allPoss = get_sets_of_5(sp, trks)
+	#print(type(allPoss))
+	#print(len(allPoss[0]))
 	#ans = get_sets_of_5(sp, [0, 1, 2, 3, 4, 5, 6])
 	#print(len(ans))
-	print(len(ans))
-	ids = audio_id(sp, trks)
-	features = audio_analysis(sp, ids)
-	#print(ids)
-	attributes = ["acousticness", "danceability", "duration_ms", "energy", "instrumentalness", "key", "liveness", "loudness", "mode", "speechiness", "tempo", "time_signature", "valence"]
-	lenat = len(attributes)
-	recats = []
-	for i in range(lenat):
-		recats += [get_attributes(sp, features, attributes[i])]
-	recats += [get_popularity(sp, trks)] 
+	ans = get_recs_all(sp, allPoss)
+	print(ans)
 	#print(recats)
 	#print(features)
 	#print(len(features))
